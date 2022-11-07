110a802215a8d56b951cd32b5a09fd29a9eec123
Arijit Patra <106462831+arijit232000@users.noreply.github.com>
2022-11-07 22:27:29 +0530

    Update WatchDetails.java

--- WatchDetails.java
+++ WatchDetails.java
@@ -1,7 +1,43 @@
 package com.arijit.watchpack;
 
-import java.util.Scanner;
+public class Watch {
+	private String name;
+	private double price;
+	private String color;
+	
+	public Watch(String name, double price, String color) {
+	
+		this.name = name;
+		this.price = price;
+		this.color = color;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public void displayDetails() {
+		System.out.println("Model Name of Watch: "+name);
+		System.out.println("price have to Pay: "+price);
+		System.out.println("Color: "+color);
+	}
 
+}
+
+import java.util.Scanner;
 public class WatchDetails {
 
 	public static void main(String[] args) {
@@ -9,8 +45,10 @@ public class WatchDetails {
 		Watch w = null;
 		boolean flag = true;
 		do {
-			System.out.println("   ************************\n +< WELCOME TO WATCH WORLD >+\n   ************************\n Enter Options\n ---------------- \n"
+		
+	System.out.println("   ************************\n +< WELCOME TO WATCH WORLD >+\n   ************************\n Enter Options\n ---------------- \n"
 					+ " 1.Add Watch \n 2.Display watch\n 3.Display Price\n 4.Modifie Color\n 5.Exit");
+			
 			int option = scn.nextInt();
 			switch(option) {
 			case 1:{
